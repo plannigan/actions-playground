b2d3b35e16681858e684f7fc5a8bb5e7a40f33b9
Patrick Lannigan <plannigan@wayfair.com>
2021-02-01 09:56:30 -0500

    Drop leading "v" from tag name to have just the version number

--- .github/actions/publish-docs-with-mike/action.sh
+++ .github/actions/publish-docs-with-mike/action.sh
@@ -22,7 +22,8 @@ elif [[ "${GITHUB_EVENT_NAME:-}" != "release" ]]; then
   echo "::error::new_version can only be used for release events."
   exit 1
 else
-  "${GITHUB_ACTION_PATH}/update_docs_for_version.sh" "${RELEASE_TAG}"
+  # drop leading "v" from tag name to have just the version number
+  "${GITHUB_ACTION_PATH}/update_docs_for_version.sh" "${RELEASE_TAG:1}"
 fi
 echo "git push origin gh-pages"
 git push origin gh-pages
